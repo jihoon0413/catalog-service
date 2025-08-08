83c1953e3fcc1859aa8184553b061f7f693d9367
jihoon <wlgnstls0413@naver.com>
2025-08-08 17:27:38 +0900

    CH3.4-validator를 활용한 단위 테스트

--- src/main/java/com/polarbookshop/catalog_service/domain/Book.java
+++ src/main/java/com/polarbookshop/catalog_service/domain/Book.java
@@ -1,24 +1,25 @@
 package com.polarbookshop.catalog_service.domain;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 
 public record Book(
-        @NotBlank(message = "The book ISBN must be defined.")
+        @NotNull(message = "The book ISBN must be defined.")
         @Pattern(
                 regexp = "^([0-9]{10}|[0-9]{13})$",
                 message = "The ISBN format must be valid."
         ) //ISBN 형식에 맞는 규칙 설정
         String isbn,
 
-        @NotBlank(message = "The book title must be defined.")
+        @NotNull(message = "The book title must be defined.")
         String title,
 
-        @NotBlank(message = "The book author must be defined.")
+        @NotNull(message = "The book author must be defined.")
         String author,
 
-        @NotBlank(message = "The book price must be defined.") // null이면 안됨
+        @NotNull(message = "The book price must be defined.") // null이면 안됨
         @Positive(message = "The book price must be greater than zero") // 0 보다 큰 수
         Double price
 ) {
--- src/main/java/com/polarbookshop/catalog_service/persistence/InMemoryBookRepository.java
+++ src/main/java/com/polarbookshop/catalog_service/persistence/InMemoryBookRepository.java
@@ -2,11 +2,13 @@ package com.polarbookshop.catalog_service.persistence;
 
 import com.polarbookshop.catalog_service.domain.Book;
 import com.polarbookshop.catalog_service.domain.BookRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class InMemoryBookRepository implements BookRepository {
     private static final Map<String, Book>  books = new ConcurrentHashMap<>();
 
